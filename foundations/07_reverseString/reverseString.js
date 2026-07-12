8d6b193577205a69872517991c2e1034348f2d99
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-07-12 21:22:32 +0800

    Feat: add string reverser function

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let letters = []
+  for (let i = 0; i < str.length; i++) {
+    letters.push(str[i])
+  }
+  return letters.reverse().join("")
 };
 
 // Do not edit below this line
