7a5ce87da37d933a8a963ff46324c8f953e0a062
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-07-12 21:08:49 +0800

    Feat: add string repeater function

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, dupe) {
+  let fullStr = ""
+  if (!(dupe < 0)) {
+    for (let i = 0; i < dupe; i++) {
+      fullStr += str
+    }
+    return fullStr
+  } else {
+    return 'ERROR'
+  }
+  
 };
 
 // Do not edit below this line
