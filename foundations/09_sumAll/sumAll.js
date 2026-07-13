d130c1380179515056d4908bfde9faceedfd950a
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-07-13 16:06:51 +0800

    Feat: improve sort with comparator

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(x, y) {
   if (!(Number.isInteger(x)) || !(Number.isInteger(y)) ||x < 0 || y < 0) return "ERROR"; 
-  const range = [x, y].sort();
+  const range = [x, y].sort((a, b) => a - b);
   let total = 0
   for (let i = range[0]; i <= range[1]; i++) {
     total += i
