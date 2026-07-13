39e1fec8dcf14d0106bf9308cead5ce1cfac493a
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-07-13 17:02:21 +0800

    Feat: add celsius and fahrenheit converter

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  return Math.round((5 / 9 * (fahr - 32)) * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  return Math.round(((9 / 5 * cel) + 32) * 10) / 10
 };
 
 // Do not edit below this line
